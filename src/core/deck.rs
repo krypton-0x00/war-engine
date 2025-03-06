ed6eab041758b4fcfbedd02a4f96b1f148a0ce58
krypton-0x00 <shakirgulzar0x00@gmail.com>
2025-03-07 03:27:38 +0530

    Feat: Optimized Deck creation

--- src/core/deck.rs
+++ src/core/deck.rs
@@ -9,50 +9,34 @@ pub struct Deck {
 impl Deck {
     pub fn new() -> Self {
         let mut cards = vec![];
-        for i in 2..15 {
-            let rank: String = match i {
+
+        let mut value = 2;
+        for i in 2..54 {
+            if cards.len() == 13 || cards.len() == 26 || cards.len() == 39 {
+                value = 2
+            }
+
+            let rank: String = match value {
                 11 => "J".to_string(),
                 12 => "Q".to_string(),
                 13 => "K".to_string(),
                 14 => "A".to_string(),
                 x => x.to_string(),
             };
-            let new_card = Card::new(rank, i, Suite::SPADE, Color::BLACK);
-            cards.push(new_card);
-        }
-        for i in 2..15 {
-            let rank: String = match i {
-                11 => "J".to_string(),
-                12 => "Q".to_string(),
-                13 => "K".to_string(),
-                14 => "A".to_string(),
-                x => x.to_string(),
+            let (suite, color) = match i {
+                2..=15 => (Suite::SPADE, Color::BLACK),
+                15..=27 => (Suite::CLUB, Color::BLACK),
+                27..=40 => (Suite::HEART, Color::RED),
+                40..=53 => (Suite::DIAMOND, Color::RED),
+                //NOOB THING
+                _ => panic!("Out of scope"),
             };
-            let new_card = Card::new(rank, i, Suite::CLUB, Color::BLACK);
+
+            let new_card = Card::new(rank, value, suite, color);
             cards.push(new_card);
+            value += 1;
         }
-        for i in 2..15 {
-            let rank: String = match i {
-                11 => "J".to_string(),
-                12 => "Q".to_string(),
-                13 => "K".to_string(),
-                14 => "A".to_string(),
-                x => x.to_string(),
-            };
-            let new_card = Card::new(rank, i, Suite::DIAMOND, Color::RED);
-            cards.push(new_card);
-        }
-        for i in 2..15 {
-            let rank: String = match i {
-                11 => "J".to_string(),
-                12 => "Q".to_string(),
-                13 => "K".to_string(),
-                14 => "A".to_string(),
-                x => x.to_string(),
-            };
-            let new_card = Card::new(rank, i, Suite::HEART, Color::RED);
-            cards.push(new_card);
-        }
+        dbg!(&cards);
         Self {
             total: 52,
             cards: cards,
